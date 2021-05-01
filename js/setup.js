50d215ef88848dd887afe889d9016fd97e091a63
Alex ZJ Zhou <47014551+zjalexzhou@users.noreply.github.com>
2021-05-01 23:16:01 +0800

    Setup
    Web-frame basics & Data Upload

---
+++ js/setup.js
@@ -0,0 +1,17 @@
+var mapOpts = {
+    center:[29.86045, -95.36978], // set map centered around Houston
+    zoom: 10
+}
+
+var map = L.map('map', mapOpts);
+map.initialBounds = map.getBounds(); // record the initial bounds mapped
+
+var tileOpts = {
+    attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    subdomains: 'abcd',
+    minZoom: 0,
+    maxZoom: 20,
+    ext: 'png'
+};
+
+var Stamen_TonerLite = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', tileOpts).addTo(map);
