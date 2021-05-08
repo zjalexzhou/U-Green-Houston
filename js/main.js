3637643a7646516a8e224b597863a9b335fdc2c7
Alex ZJ Zhou <47014551+zjalexzhou@users.noreply.github.com>
2021-05-08 10:16:35 +0800

    Update map layers

--- js/main.js
+++ js/main.js
@@ -32,7 +32,7 @@ Global Variables
 let parkPath = "https://gist.githubusercontent.com/zjalexzhou/98a3cb5960a333c3a73556bb4cfa54eb/raw/ff2425cbca1a764c8c79bc0cc1b8ad5ea02d423b/houstonParksPoint.geojson";
 let parkServePath = "https://gist.githubusercontent.com/zjalexzhou/7f937f2d8d358238a00a312626e290f6/raw/e7221e9fc4e23d0779d39dae4a8bc39097e606e1/houstonParkServe.geojson";
 let parkSectorsPath = "https://gist.githubusercontent.com/zjalexzhou/7f937f2d8d358238a00a312626e290f6/raw/e7221e9fc4e23d0779d39dae4a8bc39097e606e1/houstonParkSectorData.geojson";
-let superNhoodsPath = "https://gist.githubusercontent.com/zjalexzhou/7f937f2d8d358238a00a312626e290f6/raw/e7221e9fc4e23d0779d39dae4a8bc39097e606e1/houstonSuperNhoodsData.geojson";
+let superNhoodsPath = "https://gist.githubusercontent.com/zjalexzhou/9c9dad4e0520820598e0ffbb24aec660/raw/79b307598123826edcc90cf24b3d5081d855e7e1/superNhoodDataNARemoved.geojson";
 let tractsPath = "https://gist.githubusercontent.com/zjalexzhou/b3ac91352a22d127ad80635a84f49c04/raw/37db082cd4f779f067b5591eea2e89574ceda1aa/houstonTractsCleanedPPI.geojson";
 
 // For Data Storage
@@ -45,25 +45,12 @@ let tracts;
 let featureGroupId = [];
 let featureGroup = [];
 let markers = [];
+let jenk_class=[];
 map.initialBounds = map.getBounds(); // record the initial bounds mapped
 
 /* ===============================
 Operation Control Configuration
 =============================== */
-
-let menuControl = function(){
-    let a = document.getElementById("content");
-    //if sidebar is open
-    if (a.style.marginLeft == "300px") {
-      //change left margin of content
-      a.style.marginLeft= "0";
-      $('#menuButton').text('Show Menu')
-    } else {
-      //change left margin of content
-      a.style.marginLeft= "300px";
-      $('#menuButton').text('Hide Menu')
-    }
-}
 
 let downloadLink = function(){
   if (confirm("A pop-up window will direct you to Gist. Continue? (Please enable pop-ups from this site in your browser. Thanks :)")){
@@ -86,13 +73,34 @@ Event Functions Configuration
 ============================= */
 
 let tearDown = function(){
-  // remove all plotted data in prep for building the page with new filters etc
-  map.removeLayer(featureGroup)
+  // remove all plotted data in prep for building the page with new data/styling etc
+  _.each(featureGroup, function(e){
+    map.removeLayer(e);
+  });
 }
 
 let buildPage = function(pageDefinition, data){
     // build up a "slide" given a page definition
-
+    switch(pageDefinition.pgCode){
+      case 0:
+        jenk_class = turf.jenks(tracts, "final_score", 5)
+        $('#legend-title-show').text('Park Priority Index (0~100)')
+        $('#colorRamp2').hide()
+        $('#colorRamp1').show()
+        break;
+      case 1:
+        jenk_class = turf.jenks(superNeighborhoods, "AreaServedRatio", 5)
+        $('#legend-title-show').text('Park Service Area Coverage (0~1)')
+        $('#colorRamp1').hide()
+        $('#colorRamp2').show()
+        break;
+      case 2:
+        jenk_class = turf.jenks(parkSectors, "AreaServedRatio", 5)
+        $('#legend-title-show').text('Park Service Area Coverage (0~1)')
+        $('#colorRamp1').hide()
+        $('#colorRamp2').show()
+        break;
+    }
     if(pageDefinition.filter === undefined){
         theFilter = function() {return true};
     } else {
@@ -106,8 +114,7 @@ let buildPage = function(pageDefinition, data){
     tmpFeatureGroup.eachLayer(eachFeatureFunction);
     featureGroupId.push(tmpFeatureGroup._leaflet_id);
     featureGroup.push(tmpFeatureGroup);
-
-    $('#'+pageDefinition.legendId).show()
+    // $('#'+pageDefinition.legendId).show()
     // featureGroup.eachLayer(eachFeatureFunction);
 }
 
@@ -115,7 +122,6 @@ let removeMarkers = function(){
   _.each(markers, function(e){
     map.removeLayer(e)
   })
-
 }
 
 let fullExtent = function(){
@@ -135,118 +141,168 @@ var parkIcon = L.icon({
       popupAnchor:  [-3, -76]
 });
 
-// getDesSats(dataset, property)
-// This function generates 3 types of descriptive statistics (min, median, and max) 
-// for a particular property of the input dataset.
-// Return:
-// An array of min, median, and max values for the specified property as well as the number of NA values for that property
-let getDesStats = function(dataset, property){
-  let tmpDesStats = [];
-  let numOfNAs = 0;
-  _.each(dataset, function(e){
-    if(e[property] >= 0){
-      tmpDesStats.push(e[property])
-    } else {
-      numOfNAs += 1;
-    }
-  })
-  minVal = math.min(tmpDesStats)
-  medVal = math.median(tmpDesStats)
-  maxVal= math.max(tmpDesStats)
-  return [minVal, medVal, maxVal, numOfNAs]
-}
-
-// getChromeColorRamp(data, min, median, max)
-
-var getChromeColorRamp = function(data, min, median, max){
-  startColor = '#000000'
-  medColor = '#ffff00'
-  endColor = '#0000ff'
-  colorRamp = chroma.scale([startColor, medColor, endColor]).domain([min, median, max]);
-  return colorRamp(data).hex();
-} 
-
-var getLegendContent = function(data){
-  var s = '';
-  var dom = d.domain ? d.domain() : [0,1],
-      dmin = Math.min(dom[0], dom[dom.length-1]),
-      dmax = Math.max(dom[dom.length-1], dom[0]);
-  for (var i=0;i<=100;i++) {
-      s += '<span class="grad-step" style="background-color:'+d(dmin + i/100 * (dmax - dmin))+'"></span>';
-  }
-  s += '<span class="domain-min">'+dmin+'</span>';
-  s += '<span class="domain-med">'+((dmin + dmax)*0.5)+'</span>';
-  s += '<span class="domain-max">'+dmax+'</span>';
-  return '<div class="gradient">'+s+'</div>';
-}
-
-
-let currentPage = 0;
-
-
-let greenParkStyle = function(){
-  return {color: 'Green'}
-}
-
-let blueServiceAreaStyle = function(){
-  return {color: 'Blue'}
-}
-
-var parkAccess = {
-  title: "Park Access",
-  content: "\"In these challenging times, access to the outdoors is more important than ever. And yet, across the country, more than 100 million people don't have a park within a 10-minute walk of home.\"\n\nThe first map presents the percentage of area covered by the 10-minute-walk service areas of parks for each census tract in Houston, TX.",
-  dataForViz: tracts,//[park, parkServe],
-  style: [greenParkStyle(), blueServiceAreaStyle()],
-}
-
 var PPIPageDef = {
+  pgCode: 0,
   title: "Park Priority Index",
   content: "A measurement of park demand for each census tract to prioritz certain tracts for parkland planning and funding allocation.",
   style: function(feature){
       PPI = feature.properties.final_score;
-      $('#jk1').text(math.round(PPI_jenk_class[0],1))
-      $('#jk2').text(math.round(PPI_jenk_class[1],1))
-      $('#jk3').text(math.round(PPI_jenk_class[2],1))
-      $('#jk4').text(math.round(PPI_jenk_class[3],1))
-      $('#jk5').text(math.round(PPI_jenk_class[4],1))
-      $('#jk6').text(math.round(PPI_jenk_class[5],1))
-      if (PPI >= PPI_jenk_class[0] & PPI < PPI_jenk_class[1]){
-        feature.properties['priority'] = 'Well Below Average'
-        return{color: "#A4D4B4"};
+      $('p[name="jk1"]').text(math.round(jenk_class[0],1))
+      $('p[name="jk2"]').text(math.round(jenk_class[1],1))
+      $('p[name="jk3"]').text(math.round(jenk_class[2],1))
+      $('p[name="jk4"]').text(math.round(jenk_class[3],1))
+      $('p[name="jk5"]').text(math.round(jenk_class[4],1))
+      $('p[name="jk6"]').text(math.round(jenk_class[5],1))
+      if (PPI >= jenk_class[0] & PPI < jenk_class[1]){
+        feature.properties['priority'] = '<Strong style="color:green;">Well Below Average</strong>'
+        return{color: "green"};
       }
-      else if (PPI >= PPI_jenk_class[1] & PPI < PPI_jenk_class[2]){
-        feature.properties['priority'] = 'Below Average'
-        return{color:"#FFCF9C"};
+      else if (PPI >= jenk_class[1] & PPI < jenk_class[2]){
+        feature.properties['priority'] = '<Strong style="color:#045664;">Below Average</strong>'
+        return{color:"#045664"};
       }
-      else if (PPI >= PPI_jenk_class[2] & PPI < PPI_jenk_class[3]){
-        feature.properties['priority'] = 'Average'
+      else if (PPI >= jenk_class[2] & PPI < jenk_class[3]){
+        feature.properties['priority'] = '<Strong style="color:#B96D40;">Average</strong>'
         return{color:"#B96D40"}
       }
-      else if (PPI >= PPI_jenk_class[3] & PPI < PPI_jenk_class[4]){
-        feature.properties['priority'] = 'Above Average'
+      else if (PPI >= jenk_class[3] & PPI < jenk_class[4]){
+        feature.properties['priority'] = '<Strong style="color:#CA054D;">Above Average</strong>'
         return{color:"#CA054D"}
       }
-      else if (PPI >= PPI_jenk_class[4]){
-        feature.properties['priority'] = 'Well Above Average'
+      else if (PPI >= jenk_class[4]){
+        feature.properties['priority'] = '<Strong style="color:#3B1C32;">Well Above Average</strong>'
         return{color:"#3B1C32"}
       }
     },
-  legendId: 'initial-legend'
 }
 
-slides = [parkAccess]
-PPI_jenk_class=[];
+let SuperNhoodsServe = {
+  pgCode: 1,
+  title: "Park Service Area Coverage for each Super Neighborhood ",
+  content: "",
+  style: function(feature){
+    ASR = feature.properties.AreaServedRatio;
+    $('p[name="jk1"]').text(math.round(jenk_class[0],1))
+    $('p[name="jk2"]').text(math.round(jenk_class[1],1))
+    $('p[name="jk3"]').text(math.round(jenk_class[2],1))
+    $('p[name="jk4"]').text(math.round(jenk_class[3],1))
+    $('p[name="jk5"]').text(math.round(jenk_class[4],1))
+    $('p[name="jk6"]').text(math.round(jenk_class[5],1))
+    if (ASR >= jenk_class[0] & ASR < jenk_class[1]){
+      feature.properties['coverage'] = '<Strong style="color:darkgoldenrod;">Well Below Average</strong>'
+      return{color: "darkgoldenrod"};
+    }
+    else if (ASR >= jenk_class[1] & ASR < jenk_class[2]){
+      feature.properties['coverage'] = '<Strong style="color:orange;">Below Average</strong>'
+      return{color:"orange"};
+    }
+    else if (ASR >= jenk_class[2] & ASR < jenk_class[3]){
+      feature.properties['coverage'] = '<Strong style="color:#41b6c4;">Average</strong>'
+      return{color:"#41b6c4"}
+    }
+    else if (ASR >= jenk_class[3] & ASR < jenk_class[4]){
+      feature.properties['coverage'] = '<Strong style="color:#2c7fb8;">Above Average</strong>'
+      return{color:"#2c7fb8"}
+    }
+    else if (ASR >= jenk_class[4]){
+      feature.properties['coverage'] = '<Strong style="color:darkgreen;">Well Above Average</strong>'
+      return{color:"darkgreen"}
+    }
+  },
+}
+
+let SuperNhoodsBldgDensity = {
+
+}
+
+let ParkSectorServe = {
+  pgCode: 2,
+  title: "Park Service Area Coverage for each Park Sector ",
+  content: "",
+  style: function(feature){
+    ASR = feature.properties.AreaServedRatio;
+    $('p[name="jk1"]').text(math.round(jenk_class[0],1))
+    $('p[name="jk2"]').text(math.round(jenk_class[1],1))
+    $('p[name="jk3"]').text(math.round(jenk_class[2],1))
+    $('p[name="jk4"]').text(math.round(jenk_class[3],1))
+    $('p[name="jk5"]').text(math.round(jenk_class[4],1))
+    $('p[name="jk6"]').text(math.round(jenk_class[5],1))
+    if (ASR >= jenk_class[0] & ASR < jenk_class[1]){
+      feature.properties['coverage'] = '<Strong style="color:darkgoldenrod;">Well Below Average</strong>'
+      return{color: "darkgoldenrod"};
+    }
+    else if (ASR >= jenk_class[1] & ASR < jenk_class[2]){
+      feature.properties['coverage'] = '<Strong style="color:orange;">Below Average</strong>'
+      return{color:"orange"};
+    }
+    else if (ASR >= jenk_class[2] & ASR < jenk_class[3]){
+      feature.properties['coverage'] = '<Strong style="color:#41b6c4;">Average</strong>'
+      return{color:"#41b6c4"}
+    }
+    else if (ASR >= jenk_class[3] & ASR < jenk_class[4]){
+      feature.properties['coverage'] = '<Strong style="color:#2c7fb8;">Above Average</strong>'
+      return{color:"#2c7fb8"}
+    }
+    else if (ASR >= jenk_class[4]){
+      feature.properties['coverage'] = '<Strong style="color:darkgreen;">Well Above Average</strong>'
+      return{color:"darkgreen"}
+    }
+  },
+}
+
+let ParkSectorBldgDensity = {
+
+}
 
 let eachFeatureFunction = function(layer) {
-  layer.bindTooltip("Park Priority: "+ layer.feature.properties.priority + '<br>\n' + layer.feature.properties.NAME)
+  if(layer.feature.properties.final_score >= 0){
+    layer.bindTooltip("Park Priority: "+ layer.feature.properties.priority + '<br>\n' + layer.feature.properties.NAME)
+  } 
+  else if(layer.feature.properties.coverage) {
+    if(layer.feature.properties.SNBNAME)
+      layer.bindTooltip("Park Coverage: "+ layer.feature.properties.coverage + '<br>\n' + layer.feature.properties.SNBNAME)
+    else if(layer.feature.properties.SECTOR) {
+      layer.bindTooltip("Park Coverage: "+ layer.feature.properties.coverage + '<br>\n Park Sector ' + 
+      layer.feature.properties.SECTOR + " - " + layer.feature.properties.NAME)
+    }
+  }
   layer.on('click', function (event) {
     console.log(layer)
+    console.log(layer.feature.properties.coverage)
     map.fitBounds(event.target.getBounds());
     _.each(park.features, function(e){
       // console.log(e)
       markers.push(L.marker([e.geometry.coordinates[1], e.geometry.coordinates[0]], {icon: parkIcon}).addTo(map))
     })
   })
+}
+
+let eachParkMarkerFunction
+
+assignClickListener("myRadio1", onRadioClick);
+assignClickListener("myRadio2", onRadioClick);
+assignClickListener("myRadio3", onRadioClick);
+
+function assignClickListener(id, listener) {
+  document.getElementById(id).addEventListener("click", listener);
+}
+
+function onRadioClick(event) {
+  var target = event.currentTarget,
+    selectedIndex = target.id;
+    tearDown();
+    console.log(selectedIndex)
+  switch (selectedIndex) {
+    case "myRadio1":
+      buildPage(PPIPageDef, tracts)
+      break;
+    case "myRadio2":
+      buildPage(SuperNhoodsServe, superNeighborhoods)
+      break;
+    case "myRadio3":
+      buildPage(ParkSectorServe, parkSectors)
+      break;
+  };
 }
 
   /* ========
@@ -263,7 +319,6 @@ $(document).ready(function(){
             superNeighborhoods = JSON.parse(json);
             $.ajax(tractsPath).done(function(json){
               tracts = JSON.parse(json);
-              PPI_jenk_class = turf.jenks(tracts, "final_score", 5)
               buildPage(PPIPageDef, tracts)
               // parkViz = L.geoJson(park).addTo(map)
             });
